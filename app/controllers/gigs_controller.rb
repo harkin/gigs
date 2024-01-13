0c6c8ffb813ac79e6182eb9a7ffeb2ec69669628
Henry Larkin <hfglarkin@gmail.com>
2024-01-13 23:38:39 +0000

    Fix having no last refreshed at timestamp

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -3,6 +3,7 @@ class GigsController < ApplicationController
     puts "hello"
     events_by_year = Event.order(:event_date).group_by {|event| event.event_date.year}
     @events = Hash[events_by_year.map { |k, events| [k, events.group_by { |event| event.event_date.month }] }]
+    @last_refreshed_at = Refresh.last&.last_refresh_at
   end
 
   def refresh
