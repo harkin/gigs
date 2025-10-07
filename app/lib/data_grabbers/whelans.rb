1b307ff892b70f5cbc065bfb8ce4526782d83517
Henry <harkin@users.noreply.github.com>
2025-10-07 17:52:57 +0100

    whelans now grabs events for hte next 12 months

--- app/lib/data_grabbers/whelans.rb
+++ app/lib/data_grabbers/whelans.rb
@@ -8,22 +8,41 @@ module DataGrabbers
 
       events = []
 
-      response = Faraday.get(EVENTS_URL)
-      document = Nokogiri::HTML(response.body)
-      articles = document.css("article.desk")
+      next_page = EVENTS_URL
 
-      articles.each do |article|
-        title_element = article.css("h3 a").first
+      12.times do
+        response = Faraday.get("#{next_page}")
+        document = Nokogiri::HTML(response.body)
+        events_html = document.css("article.desk")
+
+        extract_events_from_html(events_html, events)
+
+        next_page = document.css("header nav").last.css("a").last.attribute("href").to_s + "/"
+      end
+
+      ActiveRecord::Base.transaction do
+        Event.where(venue: :whelans).delete_all
+        Event.insert_all(events)
+      end
+
+      puts "Finished grabbing #{events.count} Whelans events in #{Time.now.to_i - start_time} seconds"
+
+      events
+    end
+
+    def self.extract_events_from_html(events_html, events)
+      events_html.each do |event_html|
+        title_element = event_html.css("h3 a").first
         title = title_element.text.strip
         more_info = title_element.attribute("href")&.value
 
-        date_text = article.css("li").find { |li| li.text.include?("@") }&.text&.strip
+        date_text = event_html.css("li").find { |li| li.text.include?("@") }&.text&.strip
 
         # dates look like "Tuesday, 7th of October @ 8:00 PM"
         # the 'of' interferes with parsing
         date_text.gsub!("of", "")
 
-        ticket_element = article.css("a.tickets").first
+        ticket_element = event_html.css("a.tickets").first
         ticket = ticket_element&.attribute("href")&.value
 
         event_date = Time.parse(date_text)
@@ -40,14 +59,6 @@ module DataGrabbers
         )
       end
 
-      ActiveRecord::Base.transaction do
-        Event.where(venue: :whelans).delete_all
-        Event.insert_all(events)
-      end
-
-      puts "Finished grabbing #{events.count} Whelans events in #{Time.now.to_i - start_time} seconds"
-
-      events
     end
 
   end
